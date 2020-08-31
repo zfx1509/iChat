59d494dc9e0898ca4a528bbae5726dd0c92ed1d4
zhangfengxiang <zfx1509@163.com>
2020-08-31 22:58:34 +0800

    完成登录

--- src/main/java/com/cnorz/controller/UserController.java
+++ src/main/java/com/cnorz/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.cnorz.controller;
 
 import com.cnorz.pojo.Users;
+import com.cnorz.pojo.vo.UsersVO;
+import com.cnorz.service.UserService;
 import com.cnorz.utils.JSONResult;
+import com.cnorz.utils.MD5Utils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("u")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/registOrLogin")
-    public JSONResult registOrLogin(@RequestBody Users user) {
+    public JSONResult registOrLogin(@RequestBody Users user) throws Exception{
         // 校验用户名、密码不为空
         if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
             return  JSONResult.errorMsg("用户名及密码不能为空！");
         }
 
-        return JSONResult.ok();
+        // 判断用户名是否存在
+        boolean usernameIsExist = userService.queryUsernameIsExist(user.getUsername());
+        Users userResult = null;
+
+        if(usernameIsExist) {
+            // 登录
+            userResult = userService.queryUserForLogin(user.getUsername(),
+                    MD5Utils.getMD5Str(user.getPassword()));
+            if(userResult == null) {
+                return JSONResult.errorMsg("用户名或密码错误");
+            }
+        } else {
+            // 注册
+            user.setNickname(user.getUsername());
+            user.setFaceImage("");
+            user.setFaceImageBig("");
+            user.setPassword(MD5Utils.getMD5Str(user.getPassword()));
+            userResult = userService.saveUser(user);
+        }
+        UsersVO userVO = new UsersVO();
+        BeanUtils.copyProperties(userResult, userVO);
+
+        return JSONResult.ok(userVO);
     }
 }
---
+++ src/main/java/com/cnorz/service/impl/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.cnorz.service.impl;
+
+import com.cnorz.mapper.UsersMapper;
+import com.cnorz.pojo.Users;
+import com.cnorz.service.UserService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+        Users user = new Users();
+        user.setUsername(username);
+        Users result = usersMapper.selectOne(user);
+        return result != null ? true : false;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserForLogin(String username, String pwd) {
+        Example userExample = new Example(Users.class);
+        Example.Criteria criteria = userExample.createCriteria();
+
+        criteria.andEqualTo("username", username);
+        criteria.andEqualTo("password", pwd);
+
+        Users result = usersMapper.selectOneByExample(userExample);
+
+        return result;
+    }
+
+    @Override
+    public Users saveUser(Users user) {
+        String userId = sid.nextShort();
+
+        // TODO 为每个用户生成唯一二维码
+        user.setQrcode("");
+
+        user.setId(userId);
+        usersMapper.insert(user);
+
+        return user;
+    }
+
+
+}
